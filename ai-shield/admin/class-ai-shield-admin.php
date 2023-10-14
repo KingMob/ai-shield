9040444076b8c555903b15a4fd88093cabdb182b
Matthew Davidson <matthew@modulolotus.net>
2023-10-14 17:52:37 +0700

    Add link to check invisible characters

--- ai-shield/admin/class-ai-shield-admin.php
+++ ai-shield/admin/class-ai-shield-admin.php
@@ -263,6 +263,7 @@ class Ai_Shield_Admin {
 	public function plugin_action_links( $links ) {
 		$action_links = [
 			'settings' => '<a href="' . admin_url( 'options-general.php?page=ai_shield' ) . '" aria-label="' . esc_attr__( 'View AI Shield settings', 'ai-shield' ) . '">' . esc_html__( 'Settings', 'ai-shield' ) . '</a>',
+			'invisible' => '<a href="https://invisible-characters.com/view.html" aria-label="' . esc_attr__( 'View invisible characters', 'ai-shield' ) . '">' . esc_html__( 'View invisible characters', 'ai-shield' ) . '</a>',
 		];
 
 		return array_merge( $action_links, $links );
